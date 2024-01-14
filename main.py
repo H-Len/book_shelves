df31c7bf6a55b40a85214cea552dc5b39ac1ef19
H-Len <8978475+H-Len@users.noreply.github.com>
2024-01-13 16:01:27 -0800

    started adding code from TKinter library

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 from book import Book
-from shelves import to_read, currently_reading, completed_books
+from shelves import Now_reading, Past_read, Future_read
+
+def main(): 
+    num_rows = 10
+    num_cols = 3
+    margin = 50
+    screen_x = 800
+    screen_y = 600
+    # cell_size_x = (screen_x - 2 * margin) / num_cols
+    # cell_size_y = (screen_y - 2 * margin) / num_rows
+
+    print("bookshelf in progress.")
+
+
+main()
+
--- shelves.py
+++ shelves.py
@@ -1,7 +1,7 @@
 #shelves on which to store books
 
 # from typing import Any
-from tkinter import Entry, Canvas, Checkbutton, Radiobutton, Frame, RADIOBUTTON, Grab, Grid, PanedWindow, tkinter as tk
+from tkinter import Entry, Canvas, Checkbutton, Radiobutton, Frame, RADIOBUTTON, Grid, PanedWindow, Tk, BOTH
 #May use the following:
 #https://tcl.tk/man/tcl8.6/TkCmd/ttk_entry.htm#M56
 #https://tcl.tk/man/tcl8.6/TkCmd/ttk_checkbutton.htm
@@ -11,15 +11,30 @@ from tkinter import Entry, Canvas, Checkbutton, Radiobutton, Frame, RADIOBUTTON,
 #https://tcl.tk/man/tcl8.6/TkCmd/grid.htm
 #https://tcl.tk/man/tcl8.6/TkCmd/ttk_panedwindow.htm
 
-class to_read:
+class Bookshelf:
+    def __init__(self):
+        pass
+
+    def wait_for_close(self):
+        self.__running = True
+        while self.__running:
+            self.redraw()
+        print("window closed...")
+
+
+    def close(self):
+        self.__running = False
+
+class Future_read:
     def __init__(self, to_read_books):
         self.to_read_books = to_read_books
 
-class currently_reading:
+class Now_reading:
     def __init__(self, reading):
         self.reading = reading
 
-class completed_books:
+class Past_read:
     def __init__(self, book_read):
         #completed doesn't mean the entire book was read, necessarially, just that you won't be reading anymore, ever
         self.book_read = book_read
+
