edd1e7d030482ddf38eb0d90daa539ffab34a748
H-Len <8978475+H-Len@users.noreply.github.com>
2024-01-15 16:40:08 -0800

    setting up Window moved to graphics.py; imported to main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from graphics import Window 
 from book import Book
-from shelves import Now_reading, Past_read, Future_read
+# from shelves import Now_reading, Past_read, Future_read
+import sys
 
 def main(): 
     num_rows = 10
@@ -7,11 +9,14 @@ def main():
     margin = 50
     screen_x = 800
     screen_y = 600
-    # cell_size_x = (screen_x - 2 * margin) / num_cols
-    # cell_size_y = (screen_y - 2 * margin) / num_rows
+    cell_size_x = (screen_x - 2 * margin) / num_cols
+    cell_size_y = (screen_y - 2 * margin) / num_rows
 
     print("bookshelf in progress.")
 
+    sys.setrecursionlimit(10000)
+    print("BOOKs!!!!")
+    win = Window(screen_x, screen_y)
 
 main()
 
--- shelves.py
+++ shelves.py
@@ -1,30 +1,5 @@
 #shelves on which to store books
 
-# from typing import Any
-from tkinter import Entry, Canvas, Checkbutton, Radiobutton, Frame, RADIOBUTTON, Grid, PanedWindow, Tk, BOTH
-#May use the following:
-#https://tcl.tk/man/tcl8.6/TkCmd/ttk_entry.htm#M56
-#https://tcl.tk/man/tcl8.6/TkCmd/ttk_checkbutton.htm
-#https://tcl.tk/man/tcl8.6/TkCmd/ttk_radiobutton.htm
-#https://tcl.tk/man/tcl8.6/TkCmd/ttk_frame.htm
-#https://tcl.tk/man/tcl8.6/TkCmd/grab.htm#M12
-#https://tcl.tk/man/tcl8.6/TkCmd/grid.htm
-#https://tcl.tk/man/tcl8.6/TkCmd/ttk_panedwindow.htm
-
-class Bookshelf:
-    def __init__(self):
-        pass
-
-    def wait_for_close(self):
-        self.__running = True
-        while self.__running:
-            self.redraw()
-        print("window closed...")
-
-
-    def close(self):
-        self.__running = False
-
 class Future_read:
     def __init__(self, to_read_books):
         self.to_read_books = to_read_books
