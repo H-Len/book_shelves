1bc90e8a8d628bb7afd5c3587f47e0a79d25a664
H-Len <8978475+H-Len@users.noreply.github.com>
2024-01-21 21:15:28 -0800

    removed single L/R buttons; delete is functional again; WIP

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import *
-
+# import display
 
 root = tk.Tk()
 root.title("bookshelves")
@@ -8,20 +8,33 @@ root.title("bookshelves")
 # Create a frame
 frame = tk.Frame(root)
 frame.pack()
-
-title = Label(text="Past reading, Current reading, & Future reading below, respectively")
+title = Label(text="Past reading, Current reading, & Future reading")
 title.pack(side=TOP)
+# title1 = Label(root, text="Past reading")
+# title2 = Label(text="Current reading")
+# title3 = Label(text="Future reading")
+# title3.pack(side='top')
+# title2.pack(side='top', before=title3)
+# title.pack(side='top', before=title2)
+
+
 listboxes = [tk.Listbox(frame) for _ in range(3)]
-listbox = tk.Listbox(frame)
+# listbox = tk.Listbox(frame)
 l_colors = ["lightblue", "lightgreen", "pink"]
 shelf_label = ["Past read", "Currently reading", "Future read"]
+
+
+
 
 for i, listbox in enumerate(listboxes):
     listbox.insert('end', f'{shelf_label[i]}')
     listbox.pack(side='left')
     listbox.configure(background = l_colors[i])
 
-# used to move items
+
+
+#//////////////////////////////////
+# used to move items (many buttons)
 def move_item(src, dest):
     try:
         # 'active' is the line which has been selected
@@ -34,7 +47,7 @@ def move_item(src, dest):
 #bottons to move items left or right
 for i in range(3):
     # Move item to the right
-    button_right = tk.Button(root, text=f'Move {(i+1)%3}<-{i}',
+    button_right = tk.Button(root, text=f'Move {i}->{(i+1)%3}',
                              command=lambda src=listboxes[i], dest=listboxes[(i+1)%3]: move_item(src, dest))
     button_right.pack(side="left")
 
@@ -51,8 +64,8 @@ def add_item():
         entry.delete(0, tk.END)  # Clear the entry field
 
 def del_current():
-   for item in listbox.curselection():
-      listbox.delete(item)
+    for item in listbox.curselection():
+        listbox.delete(item)
 
 
 entry = tk.Entry(root)
@@ -61,14 +74,14 @@ entry.pack()
 add_button = tk.Button(root, text="Add Item", command=add_item)
 add_button.pack()
 
-remove_button = tk.Button(root, text="Drop", command=del_current)
+remove_button = tk.Button(root, text="Delete", command=del_current)
 remove_button.pack()
 
 # Create a Frame for border
 border_color = Frame(root, background="red")
  
 # Label Widget inside the Frame
-label = Label(border_color, text="Move book to right most box and select it before clicking drop", bd=0)
+label = Label(border_color, text="Move book to right most box and select it before clicking delete", bd=5)
  
 # Place the widgets with border Frame
 label.pack(padx=1, pady=1)
